319169edcfd0109008a0f7f6d882473f7d7c206d
daniela <daniela.acuna93@gmail.com>
2020-10-18 21:09:23 -0500

    timer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-var timerEl = document.getElementById('countdown');
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinute = 60 ,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+};
+var c = 0;
+function myCounter() {
+  document.getElementById("myTimer").innerHTML = ++c;
+}
